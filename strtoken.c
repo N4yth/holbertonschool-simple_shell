1898efac9c308aaac20a99574d9043649c88078a
N4yth <nathan.dupuis31@gmail.com>
2025-04-17 09:48:44 +0200

    add: add a input variable to set the separator use in the for

--- strtoken.c
+++ strtoken.c
@@ -9,13 +9,14 @@
  * Return: always 0
  */
 
-char **strtoken(char **command, char *line)
+char **strtoken(char **command, char *line, char *separator)
 {
 	int i = 0;
 
 	line = strtok(line, "\n");
 
-	for (line = strtok(line, " ") ; line != NULL ; line = strtok(NULL, " "))
+	for (line = strtok(line, separator) ; line != NULL ;
+			line = strtok(NULL, separator))
 	{
 		command[i] = line;
 		i++;
